9b17b30d9f5cde8439664d90fff92045e5f05b00
glejeune <gregoire.lejeune@free.fr>
2016-05-31 17:25:44 +0200

    Release 1.1.0

--- Makefile
+++ Makefile
@@ -1,8 +1,15 @@
 PROJECT = cowboy_default_static_file
 
-DEPS = bucs 
-BUILD_DEPS = cowboy
+DEP_PLUGINS = mix.mk
+BUILD_DEPS = mix.mk
+ELIXIR_VERSION = ~> 1.2
+dep_mix.mk = git https://github.com/botsunit/mix.mk.git master
+
+DEPS = bucs cowboy
 dep_bucs = git https://github.com/botsunit/bucs.git 0.0.1
 dep_cowboy = git https://github.com/ninenines/cowboy.git 2.0.0-pre.3
 
 include erlang.mk
+
+release: app mix.exs
+
---
+++ mix.exs
@@ -0,0 +1,23 @@
+defmodule Cowboy.Default.Static.File.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :cowboy_default_static_file,
+     version: "1.1.0",
+     elixir: "~> 1.2",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps]
+  end
+
+  def application do
+    [applications: [:bucs,:cowboy]]
+  end
+
+  defp deps do
+    [ 
+      {:bucs, ~r/.*/, git: "https://github.com/botsunit/bucs.git", tag: "0.0.1"},  
+      {:cowboy, ~r/.*/, git: "https://github.com/ninenines/cowboy.git", tag: "2.0.0-pre.3"},
+    ]
+  end
+end
