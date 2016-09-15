d3954c411c393ada9b2342a79eaf8c375c53aea8
glejeune <gregoire.lejeune@free.fr>
2016-09-15 15:10:13 +0200

    Bump version to 1.2.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cowboy.Default.Static.File.Mixfile do
   def project do
     [
       app: :cowboy_default_static_file,
-      version: "1.2.4",
+      version: "1.2.5",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
